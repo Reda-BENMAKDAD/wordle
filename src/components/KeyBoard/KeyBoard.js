8450787628f2e434308f5c3d2cf98a3874d5b586
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2024-01-27 02:20:11 +0100

    implemented game logique and api querying

--- src/components/KeyBoard/KeyBoard.js
+++ src/components/KeyBoard/KeyBoard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import KeyBoardKey from "../KeyBoardKey/KeyBoardKey";
 import './KeyBoard.css'
-const KeyBoard = () => {
+const KeyBoard = ({onKeyClick}) => {
   const keys = [
     "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P",
     "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M",
     "erase", "W", "X", "C", "V", "B", "N", "enter"
   ];
+
+
+
   return (
     <div className="keyboard">
       {keys.map((key, index) => {
@@ -15,6 +18,7 @@ const KeyBoard = () => {
             key={index}
             kbkey={key} 
             className={(index === 20 || index === 27) ? 'colspan-2' : ''}
+            onClick={() => {onKeyClick(key)}}
           />
         );
       })}
--- src/components/KeyBoardKey/KeyBoardKey.js
+++ src/components/KeyBoardKey/KeyBoardKey.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './KeyBoardKey.css'
-const KeyBoardKey = ({kbkey, className }) => {
-  return <div className={`key ${className}`}>
+const KeyBoardKey = ({kbkey, className, onClick }) => {
+  return <div className={`key ${className}`} onClick={onClick}>
     { kbkey }
     </div>;
 };
