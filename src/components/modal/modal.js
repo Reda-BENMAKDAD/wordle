29da28ba70ac521c8ca6df763ff14152cfd8c277
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2024-02-03 01:09:14 +0100

    i forgot what i changed, sorry late commit and push

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,7 +1,9 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import './modal.css';
 const Modal = ({message}) => {
-  return <div className="modal">{message}</div>;
+  return <div className="modal">
+    {message}
+    </div>;
 };
 
 export default Modal;
--- src/pages/game.js
+++ src/pages/game.js
@@ -1,40 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/navbar/index";
 import KeyBoard from "../components/KeyBoard/KeyBoard";
 import Try from "../components/Try/index";
-import axios from "../axios";
+import instance from "../axios";
+import axios from 'axios'
 import Modal from "../components/modal/modal";
 
 function Home() {
   const [currentTry, setCurrentTry] = useState(0);
   const [tries, setTries] = useState([""]);
   const [message, setMessage] = useState(null);
-  const randomWord = "water"; // TODO: find a way to generate a random word that should be guessed
+  const [randomWord, setRandomWord] = useState(null);
+  useEffect(() => {
+    axios.get("https://random-word-api.herokuapp.com/word?length=5").then(res => {
+      setRandomWord(res.data[0]);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
   const handleKeyClick = (key) => {
     let temp_tries; // using this varibales to introduce changes without directly mutating the state
     if (key === "enter") {
       if (tries[currentTry].length === 5) {
-        axios
-          .get(tries[currentTry])
-          .then((res) => {
-            // 200 ok status means word exists
-            if (tries[currentTry] == randomWord.toUpperCase()){
-              setMessage("You Won!!!"); // TODO: add animation and styles to modal
-            } else {
+        if (tries[currentTry] === randomWord.toUpperCase()) {
+          setMessage("You Won!!!"); // TODO: add animation and styles to modal
+        } else {
+          instance
+            .get(tries[currentTry])
+            .then((res) => {
+              // 200 ok status means word exists
               temp_tries = [...tries, ""];
               setTries(temp_tries);
               setCurrentTry(currentTry + 1);
-            }
-            
-          })
-          .catch((err) => {
-            console.log(err);
-            setMessage("word not found"); // TODO: add animation and styles to modal
-
-          });
+            })
+            .catch((err) => {
+              console.log(err);
+              setMessage("word doesn't exist"); // TODO: add animation and styles to modal
+              setTimeout(() => {
+                setMessage(null);
+              }, 1500);
+            });
+        }
       }
-    } else if (key == "erase") {
+    } else if (key === "erase") {
       temp_tries = [...tries];
       temp_tries[currentTry] = temp_tries[currentTry].slice(0, -1);
       setTries(temp_tries);
@@ -49,7 +58,7 @@ function Home() {
     <>
       <Navbar />
       <div className="content">
-        { message && <Modal message={message} />}
+        {message && <Modal message={message} />}
         {tries.map((word, index) => {
           return <Try word={word} key={index} />;
         })}
